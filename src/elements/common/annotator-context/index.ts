8c77fa775be1877995991493c36c220e3a78c560
didikmulyadi <didikmulyadi12@gmail.com>
2022-08-09 23:29:25 +0700

    Add WithAnnotationsProps and withAnnotatorContext to export

--- src/elements/common/annotator-context/index.ts
+++ src/elements/common/annotator-context/index.ts
@@ -1,4 +1,6 @@
 export { default as AnnotatorContext } from './AnnotatorContext';
+export { WithAnnotationsProps } from './withAnnotations';
 export { default as withAnnotations } from './withAnnotations';
+export { WithAnnotatorContextProps } from './withAnnotatorContext';
 export { default as withAnnotatorContext } from './withAnnotatorContext';
 export * from './types';
